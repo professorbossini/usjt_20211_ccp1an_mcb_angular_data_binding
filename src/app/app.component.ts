d188a1c6f91990758182c18871b4ffd52a07438f
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-03-23 21:48:16 -0300

    Vimos ainterpolacao e event binding. Falta ver property binding e two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  nome
+  idade: number = 20
+
+  alterarNome (nomeInputEvent){
+    console.log(nomeInputEvent.target.value)
+    this.nome = nomeInputEvent.target.value;
+  }
+
+  lancarDado(){
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  adicionar (nomeInput){
+    console.log(nomeInput)
+    this.nome = nomeInput.value;
+  }
 }
